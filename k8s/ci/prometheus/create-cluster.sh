73446ec05beccb7bc3f51eee20b47853c9120637
Ilya Shevchenko <id.shev@yandex.ru>
2022-12-23 14:43:02 +0400

    docs: add prometheus install instructions

--- k8s/ci/prometheus/create-cluster.sh
+++
@@ -1,5 +0,0 @@
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-
-helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack
-helm install prometheus-pushgateway prometheus-community/prometheus-pushgateway
