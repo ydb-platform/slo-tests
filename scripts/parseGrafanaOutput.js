cc9b8a1b8a7145e3274bb41ac51d5256bff6a1b8
Ilya Shevchenko <id.shev@yandex.ru>
2023-01-18 19:28:43 +0400

    feat: add scripts to test grafana output

---
+++ scripts/parseGrafanaOutput.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+console.log("Arguments", process.argv);
+
+let [, , inputPath, outputPath] = process.argv;
+
+let results;
+try {
+  const file = JSON.parse(fs.readFileSync(inputPath));
+  results = file.results;
+} catch (error) {
+  console.error("Error while reading file", error);
+  process.exit(1);
+}
+
+const queries = Object.keys(results);
+console.log("queries", queries);
+
+const res = new Map();
+queries.map((q) => {
+  results[q].frames.map((v) => {
+    const key = Object.values(v.schema.fields[1].labels).join(";");
+    res.set(key, {
+      ...res.get(key),
+      ...v.schema.fields[1].labels,
+      [q]: v.data.values[1][0],
+    });
+  });
+});
+
+const array = Array.from(res.values());
+console.log(array);
+fs.writeFileSync(outputPath, JSON.stringify(array));
