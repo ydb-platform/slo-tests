449d00c115e5567dc4b98f581f53ce9d85511fbc
Ilya Shevchenko <id.shev@yandex.ru>
2023-05-15 22:18:11 +0400

    feat: add mutex functions

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,75 @@
 import * as core from '@actions/core'
+import {IWorkloadOptions, parseArguments} from './parseArguments'
+import {prepareK8S} from './callExecutables'
+import {obtainMutex, releaseMutex} from './mutex'
 
-async function run(): Promise<void> {
+async function main(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    let workloads: IWorkloadOptions[] = parseArguments()
 
-    core.debug(new Date().toTimeString())
+    core.info(
+      workloads
+        .map(option => {
+          let str = `Run SLO tests for #${option.id}`
+          str += option.name ? `(${option.name})\n` : '\n'
+          str += `path: '${option.path}'\n`
+          str += option.buildContext
+            ? `build context: '${option.buildContext}'\n`
+            : ''
+          str += option.buildOptions
+            ? `build options: '${option.buildOptions}'\n`
+            : ''
+          return str
+        })
+        .join('')
+    )
+    const mutexId =
+      workloads.length > 1
+        ? workloads.map(v => v.id).join('__+__')
+        : workloads[0].id
 
-    core.setOutput('time', new Date().toTimeString())
+    prepareK8S('')
+
+    await obtainMutex(mutexId, 30)
+
+    // can be parallel with next step (build workload)
+    core.info('Create cluster')
+    //
+
+    core.info('Build workload')
+    //
+
+    core.info('Create tables')
+    //
+
+    // retry on error? run in parrallel? run one by one?
+    core.info('Run workload')
+    //
+
+    // run in parralel with workload
+    core.info('Run error scheduler')
+    //
+
+    core.info('Check results')
+    //
+
+    core.info('Grafana screenshot')
+    //
+
+    core.info('Delete cluster')
+    //
+
+    releaseMutex()
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
-run()
+const isPostAction = !!core.getState('isPost')
+
+if (isPostAction) {
+  core.info('Cleanup should be placed here')
+  // TODO: cleanup
+} else {
+  main()
+}
---
+++ src/mutex.ts
@@ -0,0 +1,67 @@
+import * as core from '@actions/core'
+import {callKubernetes, callKubernetesPath} from './callExecutables'
+import {logGroup} from './groupDecorator'
+
+/** Is mutex busy (configmap has field busy) */
+function isBusy(name: string): boolean {
+  core.debug(`isBusy(${name})`)
+  const res = callKubernetes(`get configmaps ${name} -ojson`)
+  core.debug('isBusy result: ' + res)
+
+  const configmap = JSON.parse(res)
+  core.debug('configmap parsed: ' + JSON.stringify(configmap))
+
+  if (configmap?.data?.busy !== undefined) {
+    core.info(`Mutex locked by ${configmap?.data?.lockedBy}`)
+  }
+  return configmap?.data?.busy !== undefined
+}
+
+function setBusy(lockedBy: string) {
+  core.debug(`setBusy(${lockedBy})`)
+
+  callKubernetesPath(
+    kubectl =>
+      `${kubectl} create configmap slo-mutex --from-literal=busy=true --from-literal=lockedBy=${lockedBy} -o=yaml --dry-run=client | ${kubectl} apply -f -`
+  )
+}
+
+/**
+ * Obtain mutex
+ * @param workloadId id to mention in mutex
+ * @param timeout  timeout in minutes
+ * @param checkPeriod update period in seconds
+ */
+export function obtainMutex(
+  workloadId: string,
+  timeout: number,
+  checkPeriod: number = 20
+) {
+  return logGroup('Obtain mutex', async () => {
+    const deadline = new Date().valueOf() + timeout * 1000 * 60
+    core.info(
+      `Deadline to obtain mutex is set to: ${deadline} ( ${new Date(
+        deadline
+      ).toISOString()} )`
+    )
+    do {
+      const busy = isBusy('slo-mutex')
+      if (!busy) {
+        setBusy(workloadId)
+        core.info('Mutex obtained')
+        return
+      }
+      await new Promise(resolve => setTimeout(resolve, checkPeriod * 1000))
+    } while (new Date().valueOf() < deadline)
+    throw new Error('Deadline exceeded')
+  })
+}
+
+export function releaseMutex() {
+  return logGroup('Release mutex', () => {
+    callKubernetesPath(
+      kubectl =>
+        `${kubectl} create configmap slo-mutex -o=yaml --dry-run=client | ${kubectl} apply -f -`
+    )
+  })
+}
