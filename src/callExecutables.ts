8c827e03d903add849fa03dd47e72cdfb60db231
Ilya Shevchenko <id.shev@yandex.ru>
2023-05-26 16:15:44 +0300

    feat: add call id logging

--- src/callExecutables.ts
+++ src/callExecutables.ts
@@ -6,6 +6,7 @@ import path from 'path'
 import {logGroup} from './utils/groupDecorator'
 
 let kubectlPath: string | null = null
+let callId = 0
 
 export function prepareK8S(base64kubeconfig: string) {
   return logGroup('Prepare k8s', () => {
@@ -36,10 +37,46 @@ export function prepareK8S(base64kubeconfig: string) {
   })
 }
 
+export async function prepareAWS(
+  awsCredentialsB64: string,
+  awsConfigB64: string
+) {
+  // create ~/.aws folder
+  const kubePath = path.join(homedir(), '.aws')
+  core.info(`mkdir ${kubePath}`)
+  try {
+    mkdirSync(kubePath)
+  } catch (error: any) {
+    core.debug('error' + JSON.stringify(error))
+    if (error?.code === 'EEXIST') {
+      core.debug(kubePath + ' EEXIST')
+    } else throw error
+  }
+
+  // add awsCredentials
+  if (awsCredentialsB64.length > 0) {
+    core.debug('Get aws credentials string')
+    const awsCredentials = Buffer.from(awsCredentialsB64, 'base64').toString(
+      'utf8'
+    )
+    core.info(`Write awsCredentials to ~/.aws/credentials`)
+    writeFileSync(path.join(homedir(), '.aws/credentials'), awsCredentials)
+  }
+
+  // add awsConfig
+  if (awsConfigB64.length > 0) {
+    core.debug('Get aws credentials string')
+    const awsConfig = Buffer.from(awsConfigB64, 'base64').toString('utf8')
+    core.info(`Write awsConfig to ~/.aws/config`)
+    writeFileSync(path.join(homedir(), '.aws/config'), awsConfig)
+  }
+}
+
 export function call(command: string, secret = false) {
-  !secret && core.info(`Call command: '${command}'`)
+  const id = ++callId
+  !secret && core.info(`Call #${id} command: '${command}'`)
   const spawnResult = execSync(command, {encoding: 'utf8', stdio: 'pipe'})
-  core.debug('call result ' + spawnResult)
+  core.debug(`Call #${id} result ${spawnResult}`)
   return spawnResult
 }
 
@@ -49,11 +86,12 @@ export function callAsync(
   cwd = '.'
 ): Promise<string> {
   return new Promise((resolve, reject) => {
-    !secret && core.info(`Call async command: '${command}'`)
+    const id = ++callId
+    !secret && core.info(`Call #${id} command: '${command}'`)
     const proc = exec(command, {encoding: 'utf8', cwd})
     if (!proc.stdio || !proc.stdout || !proc.stderr) {
-      core.info(`Error in callAsync: can't spawn process`)
-      throw new Error(`Error in callAsync: can't spawn process`)
+      core.info(`Error in callAsync #${id}: can't spawn process`)
+      throw new Error(`Error in callAsync #${id}: can't spawn process`)
     }
 
     let out = '',
@@ -62,20 +100,21 @@ export function callAsync(
     proc.stderr.on('data', data => (err += data.toString()))
 
     proc.on('close', code => {
-      core.debug(`Call async code = ${code}`)
+      core.debug(`Call #${id} async code = ${code}`)
       if (code == 0) {
-        core.debug(`Call async output \n${out}`)
+        core.debug(`Call #${id} async output \n${out}`)
         resolve(out)
       } else {
-        if (secret) core.debug('Call async with secrets failed - on close')
-        else core.debug('Call async failed - on close:\n' + err)
+        if (secret)
+          core.debug(`Call #${id} async with secrets failed - on close`)
+        else core.debug(`Call #${id} async failed - on close:\n${err}`)
 
         reject(new Error(err))
       }
     })
     proc.on('error', err => {
-      if (secret) core.debug('Call async with secrets failed - on error')
-      else core.debug('Call async failed - on error:\n' + err)
+      if (secret) core.debug(`Call #${id} async with secrets failed - on error`)
+      else core.debug(`Call #${id} async failed - on error:\n${err}`)
 
       reject(err)
     })
