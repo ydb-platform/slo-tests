af1de380c8420052042900a6a98eafa2c8c2c398
Nikita <nikitalevuskin41668@gmail.com>
2024-09-19 21:50:24 +0300

    add compil files

--- src/workload.ts
+++ src/workload.ts
@@ -87,7 +87,7 @@ export function runWorkload(
   options: IWorkloadRunOptions
 ) {
   return logGroup(`Workload ${options.id} - ${command}`, async () => {
-    const containerArgs = `grpc://database-sample-grpc:2135 /Root/database-sample --table-name slo-${options.id} ${options.args}`
+    const containerArgs = `grpc://database-grpc:2135 /Root/database-sample --table-name slo-${options.id} ${options.args}`
 
     const workloadManifest = workloadManifestTemplate
       .replace(/\$\{\{LANGUAGE_ID}}/g, options.id)
