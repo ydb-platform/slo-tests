ba79e8a91aebad144cbeb67fb8d114c6ce840c39
Nikita <nikitalevuskin41668@gmail.com>
2024-09-24 16:27:33 +0300

    add compil files

--- src/main.ts
+++ src/main.ts
@@ -240,11 +240,6 @@ async function main(): Promise<void> {
         }
       }
     }
-    core.info(JSON.stringify(call("kubectl describe databases.ydb.tech")))
-    core.info(JSON.stringify(call("kubectl describe storages.ydb.tech")))
-    core.info(JSON.stringify(call("kubectl logs database-0")))
-    core.info(JSON.stringify(call("kubectl logs database-1")))
-    core.info(JSON.stringify(call("kubectl logs database-2")))
     deleteCluster()
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
