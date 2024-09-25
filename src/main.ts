7fce409baf6bff03905dadf1ba97dce6047cd6e3
Nikita <nikitalevuskin41668@gmail.com>
2024-09-25 20:47:32 +0300

    add compil files

--- src/main.ts
+++ src/main.ts
@@ -26,15 +26,7 @@ async function main(): Promise<void> {
   try {
     // test
     call('echo $PATH')
-    call(`(
-      set -x; cd "$(mktemp -d)" &&
-   OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-   ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\\(arm\\)\\(64\\)\\?.*/\\1\\2/' -e 's/aarch64$/arm64/')" &&
-   KREW="krew-\${OS}_\${ARCH}" &&
-   curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/\${KREW}.tar.gz" &&
-   tar zxvf "\${KREW}.tar.gz" &&
-   ./"\${KREW}" install krew
-    )`)
+
     call('echo $PATH')
     // call('export PATH=$PATH:${KREW_ROOT:-$HOME/.krew}/bin')
     // core.info(JSON.stringify(call('kubectl plugin list')))
