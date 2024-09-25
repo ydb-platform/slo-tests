0e2aa21d63621625c78d112603e97fae5a9c8703
Nikita <nikitalevuskin41668@gmail.com>
2024-09-25 20:01:28 +0300

    add compil files

--- src/main.ts
+++ src/main.ts
@@ -34,12 +34,12 @@ async function main(): Promise<void> {
     call('kubectl apply -f https://raw.githubusercontent.com/ihcsim/controllers/master/podlister/deployment.yaml')
 
     call(`set -x; cd "$(mktemp -d)" &&
-      OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-      ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\\(arm\\)\\(64\\)\\?.*/\\1\\2/' -e 's/aarch64$/arm64/')" &&
-      KREW="krew-\${OS}_\${ARCH}" &&
-      curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/\${KREW}.tar.gz" &&
-      tar zxvf "\${KREW}.tar.gz" &&
-      ./"\${KREW}" install krew`)
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\\(arm\\)\\(64\\)\\?.*/\\1\\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-\${OS}_\${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/\${KREW}.tar.gz" &&
+  tar zxvf "\${KREW}.tar.gz" &&
+  ./"\${KREW}" install krew`)
     call('export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"')
     call('kubectl krew update')
 
