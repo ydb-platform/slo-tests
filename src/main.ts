eb5bafa0c2ed863c0f69670042d6fcadad387613
Nikita <nikitalevuskin41668@gmail.com>
2024-09-26 23:41:57 +0300

    add compil files

--- src/main.ts
+++ src/main.ts
@@ -261,9 +261,9 @@ async function main(): Promise<void> {
     }
     //   call('kubectl port-forward svc/prometheus-operator 8080 &')
     //   const snapshot = JSON.parse(call('curl -XPOST http://localhost:8080/api/v2/admin/tsdb/snapshot'))
-    core.info(JSON.stringify(call(`kubectl get pod ${servicesPods["grafana"]} -o yaml | grep image`)))
-    call(`kubectl exec pod/${servicesPods['prometheusOperator']} -- /bin/sh -c 'prometheus snapshot create /prometheus/snapshot'`)
-    call(`kubectl cp ${servicesPods['prometheusOperator']}:/prometheus/snapshot} -c prometheus ./logs/`)
+    // core.info(JSON.stringify(call(`kubectl get pod ${servicesPods["grafana"]} -o yaml | grep image`)))
+    // call(`kubectl exec pod/${servicesPods['prometheusOperator']} -- /bin/sh -c 'prometheus snapshot create /prometheus/snapshot'`)
+    // call(`kubectl cp ${servicesPods['prometheusOperator']}:/prometheus/snapshot} -c prometheus ./logs/`)
     //   call('POD_NAME=$(kubectl get pods -l "app.kubernetes.io/name=prometheus" -o jsonpath="{.items[0].metadata.name}")')
     //   call('/home/runner/.krew/bin/kubectl-promdump -p "$POD_NAME" > "./logs/prom_data.tar.gz"')
     deleteCluster()
