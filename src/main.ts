eb82501df939ccc397f07e83e83a27ac22b64c1c
Nikita <nikitalevuskin41668@gmail.com>
2024-10-02 23:59:14 +0300

    add compil files

--- src/main.ts
+++ src/main.ts
@@ -252,9 +252,9 @@ async function main(): Promise<void> {
     call(`kubectl logs pod/database-2 > ./logs/database-2.log`)
     call(`kubectl logs pod/storage-0 > ./logs/storage-0.log`)
 
-    callAsync('timeout 60 kubectl port-forward prometheus-prometheus-0 9090')
-    call('sleep 5s')
-    call('curl -s  https://localhost:9090/api/v1/label/__name__/values > ./logs/prometheusLabelsNames.log')
+    // callAsync('timeout 60 kubectl port-forward prometheus-prometheus-0 9090')
+    // call('sleep 5s')
+    // call('curl -s  https://localhost:9090/api/v1/label/__name__/values > ./logs/prometheusLabelsNames.log')
 
     deleteCluster()
   } catch (error) {
