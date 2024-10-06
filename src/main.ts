779c97a886002bc998032fb05c297c99323d7362
Nikita <nikitalevuskin41668@gmail.com>
2024-10-06 16:46:14 +0300

    add compil files

--- src/main.ts
+++ src/main.ts
@@ -100,12 +100,12 @@ async function main(): Promise<void> {
     ])
     call('docker builder prune -f')
 
-    core.info(JSON.stringify(call('kubectl exec -i -t dnsutils -- nslookup kubernetes.default')))
-    core.info(JSON.stringify(call('kubectl exec -i -t dnsutils -- nslookup storage-0')))
-    core.info(JSON.stringify(call('kubectl exec -i -t dnsutils -- nslookup database-0')))
-    core.info(JSON.stringify(call('kubectl exec -i -t dnsutils -- nslookup database-1')))
-    core.info(JSON.stringify(call('kubectl exec -i -t dnsutils -- nslookup database-2')))
-    core.info(JSON.stringify(call(`kubectl exec -i -t dnsutils -- nslookup ${servicesPods['ydbOperator']}`)))
+    core.info(JSON.stringify(call('kubectl exec -i dnsutils -- nslookup kubernetes.default')))
+    core.info(JSON.stringify(call('kubectl exec -i dnsutils -- nslookup storage-0')))
+    core.info(JSON.stringify(call('kubectl exec -i dnsutils -- nslookup database-0')))
+    core.info(JSON.stringify(call('kubectl exec -i dnsutils -- nslookup database-1')))
+    core.info(JSON.stringify(call('kubectl exec -i dnsutils -- nslookup database-2')))
+    core.info(JSON.stringify(call(`kubectl exec -i dnsutils -- nslookup ${servicesPods['ydbOperator']}`)))
 
     /** Indicates that cluster created, some of workloads builded and it's possible to run wl */
     const continueRun =
